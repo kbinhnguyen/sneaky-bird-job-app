dcea9465186b0bf2285aa9abfd6f1f5e464db209
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-11-04 17:50:54 -0400

    changed images order

--- components/ImagesSlide.js
+++ components/ImagesSlide.js
@@ -1,16 +1,16 @@
 import { useState, useEffect, useMemo } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import first from '../public/images/SMPM3453.jpg';
-import second from '../public/images/SMPM3584-2.jpg';
-import third from '../public/images/SMPM3626.jpg';
-import fourth from '../public/images/SMPM3630.jpg';
-import fifth from '../public/images/SMPM3660.jpg';
-import sixth from '../public/images/SMPS2666.jpg';
-import seventh from '../public/images/SMPS2723.jpg';
-import eighth from '../public/images/SMPS2726.jpg';
-import ninth from '../public/images/SMPM3255.jpg';
-import tenth from '../public/images/SMPM3305-2.jpg'
+import fifth from '../public/images/SMPM3453.jpg';
+import seventh from '../public/images/SMPM3584-2.jpg';
+import fourth from '../public/images/SMPM3626.jpg';
+import eighth from '../public/images/SMPM3630.jpg';
+import ninth from '../public/images/SMPM3660.jpg';
+import second from '../public/images/SMPS2666.jpg';
+import sixth from '../public/images/SMPS2723.jpg';
+import tenth from '../public/images/SMPS2726.jpg';
+import first from '../public/images/SMPM3255.jpg';
+import third from '../public/images/SMPM3305-2.jpg';
 
 export default function ImagesSlide() {
   const srcs = useMemo(() => [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth], []);
